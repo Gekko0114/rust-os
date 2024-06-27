3cb506c0dca25e609d4ffcdf21ebd8b50fb7e10b
moriya <moriya0114.pharmacy@gmail.com>
2024-06-27 14:58:40 +0900

    cpu_exception

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
@@ -58,6 +60,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -68,5 +71,6 @@ fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
 
-
-
+pub fn init() {
+    interrupts::init_idt();
+}
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ use rust_os::println;
 pub extern "C" fn _start() {
     println!("Hello world{}", "!");
 
+    rust_os::init();
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
