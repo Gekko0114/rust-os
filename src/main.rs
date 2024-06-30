73dd80ffbbeaebf54a910364761620a8ebae5339
moriya <moriya0114.pharmacy@gmail.com>
2024-06-30 15:56:43 +0900

    double_fault_test

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,6 @@ pub extern "C" fn _start() {
     println!("Hello world{}", "!");
 
     rust_os::init();
-    fn stack_overflow() {
-        stack_overflow();
-    }
-    stack_overflow();
 
     #[cfg(test)]
     test_main();
