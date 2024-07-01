b092372ee061110f09f3c4339594151cc4f4c65c
moriya <moriya0114.pharmacy@gmail.com>
2024-07-01 14:28:18 +0900

    paging_implementation

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,28 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{memory::BootInfoFrameAllocator, println};
 
-#[no_mangle]
-pub extern "C" fn _start() {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello world{}", "!");
-
     rust_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
